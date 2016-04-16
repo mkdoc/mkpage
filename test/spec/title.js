50dec06f401507f07e90c23a409a214ce736d578
muji <freeformsystems@gmail.com>
2016-04-16 13:34:12 +0800

    Assert on title option.

---
+++ test/spec/title.js
@@ -0,0 +1,36 @@
+var expect = require('chai').expect
+  , fs = require('fs')
+  , mkast = require('mkast')
+  , parser = new mkast.Parser()
+  , mkpage = require('../../index')
+  , utils = require('../util');
+
+describe('mkpage:', function() {
+  
+  it('should create html page w/ title', function(done) {
+    var source = 'test/fixtures/page.md'
+      , target = 'target/page.json.log'
+      , data = parser.parse('' + fs.readFileSync(source))
+
+    // mock file for correct relative path
+    // mkcat normally injects this info
+    data.file = source;
+
+    var input = mkast.serialize(data)
+      , output = fs.createWriteStream(target)
+      , opts = {
+          input: input,
+          output: output,
+          title: 'TITLE'
+        };
+    
+    mkpage(opts);
+
+    output.once('finish', function() {
+      var result = utils.result(target);
+      expect(result[5].literal).to.eql('<title>TITLE</title>');
+      done();
+    })
+  });
+
+});
